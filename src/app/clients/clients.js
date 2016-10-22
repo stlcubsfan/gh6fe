a60b980f8fe3ee917cddfbbf8ea5da9edcbff21a
Andrew Weisman <andrew.weisman@wwt.com>
2016-10-22 11:18:56 -0500

    Adds client list

--- src/app/clients/clients.js
+++ src/app/clients/clients.js
@@ -6,10 +6,10 @@ angular
     controllerAs: 'Clients'
   });
 
-function makeClients($http) {
-    this.hello = 'Hello World!';
+function makeClients(clientsApi) {
+    let vm = this;
 
-    $http.get('https://gh6api.herokuapp.com/agencies?range=20&xpos=38.7591332&ypos=-90.297514').then(function (response) {
-        console.log(response);
-    })
+    clientsApi.all().then(function (response) {
+      vm.clients = response.data;
+    });
 }
