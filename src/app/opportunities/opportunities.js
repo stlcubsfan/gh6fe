5d7f548884cb3e35c6d12361881977c08e6988dc
Brian Coles <stlcubsfan@gmail.com>
2016-10-22 21:33:16 -0500

    Beginning of opps and viability score

---
+++ src/app/opportunities/opportunities.js
@@ -0,0 +1,11 @@
+angular
+  .module('app')
+  .component('opportunities', {
+    templateUrl: 'app/opportunities/opportunities.html',
+    controller: seeOpportunties,
+    controllerAs: 'Opportunities'
+  });
+
+  function seeOpportunties() {
+      
+  }
