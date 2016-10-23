c7fec98196190d3961e51776c1391be4cc7e2c63
Brian Coles <stlcubsfan@gmail.com>
2016-10-22 19:37:42 -0500

    Adding partnersApi with partners and getting opportunities for partners

---
+++ src/app/partners/partnersApi.js
@@ -0,0 +1,30 @@
+angular.module('app')
+    .factory('partnersApi', partnersApi);
+
+function partnersApi($http) {
+    let baseApi = 'http://gr6api.herokuapp.com/partners'
+    
+    return {
+        all: all,
+        one: one,
+        opportunities: opportunities,
+        clientOpportunities: clientOpportunities
+    }
+
+    function all() {
+        return $http.get(baseApi);
+    }
+
+    function one(id) {
+        return $http.get(baseApi + '/' + id);
+    }
+
+    function opportunities(partnerId) {
+        return $http.get(baseApi + '/' + partnerId + '/opportunities')
+    }
+
+    function clientOpportunities(partnerId, opportunityId) {
+        return $http.get(baseApi + '/' + partnerId + '/opportunities/' + opportunityId)
+    }
+
+}
