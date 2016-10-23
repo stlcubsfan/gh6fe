80079a5319c58efd24ce9c6d3ce8c1d28a691e2d
harrisch <charlie.harris@wwt.com>
2016-10-23 08:02:44 -0500

    Fixes some small styles

--- src/app/sidebar/sidebar.js
+++ src/app/sidebar/sidebar.js
@@ -10,19 +10,25 @@ function makeSidebar(agencyApi, $rootScope) {
     init();
 
     function init() {
-
-      agencyApi.getCurrent().then(function (response) {
-        vm.currentAgency = response;
-      });
-
       agencyApi.all().then(function (response) {
         vm.allAgencies = response.data;
+        vm.stPat = vm.allAgencies.find(agency => {
+          return agency.id === 1;
+        });
+        vm.stPatLogoUrl = vm.stPat.image_url;
+
+        agencyApi.getCurrent().then(function (response) {
+          vm.currentAgency = response;
+          vm.currentAgency.image_url = vm.currentAgency.image_url || vm.stPatLogoUrl;
+        });
       });
+
     }
 
     function setCurrenctAgency(agency) {
       agencyApi.setCurrent(agency.id);
       vm.currentAgency = agency;
+      vm.currentAgency.image_url = vm.currentAgency.image_url || vm.stPatLogoUrl;
       $rootScope.$broadcast('agency-updated', {newAgency: agency});
     }
 }
